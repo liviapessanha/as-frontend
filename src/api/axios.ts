8c84b4d1684c59975a778a94303fcaff527dad82
Livia Pessanha <liviapessanha99@gmail.com>
2024-04-26 09:51:44 -0300

    add .env and changing the file

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const req = axios.create({
-  baseURL: process.env.NEX_PUBLIC_BASE_API,
+  baseURL: process.env.NEXT_PUBLIC_BASE_API,
   headers: {
     'Content-Type': 'application/json'
   }
